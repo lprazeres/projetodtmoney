d57120055a39241967088682ae4b909624951c98
Lucas Silveira Prazeres <lprazeres@gmail.com>
2022-10-12 13:03:02 -0300

    Primeira postagem

---
+++ src/pages/Transactions/index.tsx
@@ -0,0 +1,19 @@
+import {Header} from '../../components/Header';
+import {Summary} from '../../components/Summary';
+
+export function Transactions () {
+    return (
+        <div>
+            <Header />
+            <Summary />
+
+            <table>
+                <tbody>
+                    <tr>
+                        <td></td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    )
+}
