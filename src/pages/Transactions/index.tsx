ea5e8542f7134d02de325de8e3ae641d3a4c55df
Lucas Silveira Prazeres <lprazeres@gmail.com>
2022-10-13 13:47:02 -0300

    Adicionando até a parte do server-json

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,19 +1,51 @@
-import {Header} from '../../components/Header';
-import {Summary} from '../../components/Summary';
+import { useEffect } from 'react';
+import { Header } from '../../components/Header';
+import { Summary } from '../../components/Summary';
+import { SearchForm } from './components/SearchForm';
+import { TransactionsContainer, TransactionsTable, PriceHighlight } from './styles';
 
-export function Transactions () {
+export function Transactions() {
+
+    useEffect(() => {
+        fetch('http://localhost:3333/transactions')
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+        })
+    }, [])
+
     return (
         <div>
             <Header />
             <Summary />
+            <TransactionsContainer>
 
-            <table>
-                <tbody>
-                    <tr>
-                        <td></td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
+                <SearchForm />
+                <TransactionsTable>
+                    <tbody>
+                        <tr>
+                            <td width="50%">Desenvolvimento de site</td>
+                            <td>
+                                <PriceHighlight variant="income">
+                                    R$ 12.000,00
+                                </PriceHighlight>
+                            </td>
+                            <td>Venda</td>
+                            <td>13/04/2022</td>
+                        </tr>
+                        <tr>
+                            <td width="50%">Hambúrguer</td>
+                            <td>
+                                <PriceHighlight variant="outcome">
+                                    -R$ 59,00
+                                </PriceHighlight>
+                            </td>
+                            <td>Alimentação</td>
+                            <td>10/04/2022</td>
+                        </tr>
+                    </tbody>
+                </TransactionsTable>
+            </TransactionsContainer>
+        </div >
     )
 }
